912ae07a7049f4beac41b775c09c85d3f525c8b3
8BitJonny <timon99@gmx.de>
2018-11-07 09:19:56 +0100

    Allowed Cors

--- src/main/kotlin/com/triangl/trackingIngestion/controller/TrackingIngestionController.kt
+++ src/main/kotlin/com/triangl/trackingIngestion/controller/TrackingIngestionController.kt
@@ -7,11 +7,9 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin
 @RestController
 class TrackingIngestionController (
     private val computingService: ComputingService
